eb73975558e6dd12fc87f2a022db3ff632852d55
Jaemin <jaemin0216@gmail.com>
2017-01-23 23:53:08 +0900

    Initial commit

---
+++ SettingsApp/SettingsApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SettingsApp
+//
+//  Created by LimJaemin on 2017. 1. 23..
+//  Copyright © 2017년 LimJaemin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
