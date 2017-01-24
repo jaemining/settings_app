d55728241e6fd76162f7280daeae979796df3e52
Jaemin <jaemin0216@gmail.com>
2017-01-24 17:38:43 +0900

    Add auto brightness option

--- SettingsApp/SettingsApp/DisplayTableViewController.swift
+++ SettingsApp/SettingsApp/DisplayTableViewController.swift
@@ -12,16 +12,22 @@ class DisplayTableViewController: UITableViewController {
     let defaults = UserDefaults.standard
 
     @IBOutlet var sliderDisplayBrightness: UISlider!
+    @IBOutlet var switchAutoBrightness: UISwitch!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         sliderDisplayBrightness.value = defaults.float(forKey: "brightness")
-        // Do any additional setup after loading the view, typically from a nib.
+        switchAutoBrightness.isOn = defaults.bool(forKey: "autoBrightness")
     }
-    
+
     @IBAction func changeDisplayBrightness(_ sender: UISlider) {
         UIScreen.main.brightness = CGFloat(sender.value)
         defaults.set(sender.value, forKey: "brightness")
+    }
+
+    @IBAction func changeAutoBrightness(_ sender: UISwitch) {
+        UIScreen.main.wantsSoftwareDimming = Bool(sender.isOn)
+        defaults.set(sender.isOn, forKey: "autoBrightness")
     }
 }
 
