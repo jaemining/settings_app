1d8609e68cafcc0e6940f1a9d851f61a97fd52e9
Jaemin <jaemin0216@gmail.com>
2017-01-24 16:23:21 +0900

    Add DisplayViewController.swift

---
+++ SettingsApp/SettingsApp/DisplayViewController.swift
@@ -0,0 +1,27 @@
+//
+//  DisplayViewController.swift
+//  SettingsApp
+//
+//  Created by LimJaemin on 2017. 1. 24..
+//  Copyright © 2017년 LimJaemin. All rights reserved.
+//
+
+import UIKit
+
+class DisplayViewController: UITableViewController {
+    let defaults = UserDefaults.standard
+
+    @IBOutlet var sliderDisplayBrightness: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        sliderDisplayBrightness.value = defaults.float(forKey: "brightness")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func changeDisplayBrightness(_ sender: UISlider) {
+        UIScreen.main.brightness = CGFloat(sender.value)
+        defaults.set(sender.value, forKey: "brightness")
+    }
+}
+
