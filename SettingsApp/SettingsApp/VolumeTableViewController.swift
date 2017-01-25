a030cd8428babf9c09ca8d47cb020bd0e69fe6bb
Jaemin <jaemin0216@gmail.com>
2017-01-25 16:41:17 +0900

    Delete and add comment

--- SettingsApp/SettingsApp/VolumeTableViewController.swift
+++ SettingsApp/SettingsApp/VolumeTableViewController.swift
@@ -19,10 +19,10 @@ class VolumeTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         sliderVolume.value = defaults.float(forKey: "volume")
-        // Do any additional setup after loading the view, typically from a nib.
     }
     
     @IBAction func changeVolume(_ sender: UISlider) {
+        // 코드 분리해보기 
         (MPVolumeView().subviews.filter{NSStringFromClass($0.classForCoder) == "MPVolumeSlider"}.first as? UISlider)?.setValue(sender.value, animated: false)
         
         defaults.set(sender.value, forKey: "volume")
